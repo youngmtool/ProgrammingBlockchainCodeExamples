1402c48573f520b8ea6081a4d89dfad3d1038d10
Youngmin Park <youngmtool@gmail.com>
2017-11-20 09:40:52 +0900

    Update Program.cs

--- BitcoinAddress/Program.cs
+++ BitcoinAddress/Program.cs
@@ -12,21 +12,64 @@ namespace BitcoinAddress
     {
         static void Main()
         {
-            Key privateKey = new Key(); // generate a random private key
+           RandomUtils.Random = new UnsecureRandom();
+            //===========================================================================================
+            //Part3. Bitcoin transfer
 
+            //===========================================================================================
+            //Chapter1. Bitcoin address
+
+            //===========================================================================================
+            //Section1. Bitcoin address
+
+            //Generate a random private key.
+            Key privateKey = new Key();
+            Console.WriteLine($"privateKey: {privateKey.ToString(Network.Main)}");
+            
+            //From the private key, we use a one-way cryptographic function, to generate a public key.
             PubKey publicKey = privateKey.PubKey;
-            Console.WriteLine(publicKey); // 0251036303164f6c458e9f7abecb4e55e5ce9ec2b2f1d06d633c9653a07976560c
+            Console.WriteLine($"publicKey: {publicKey}");
+            
+            Console.WriteLine(privateKey.GetBitcoinSecret(Network.Main).PubKey.GetType());
 
-            Console.WriteLine(publicKey.GetAddress(Network.Main)); // 1PUYsjwfNmX64wS368ZR5FMouTtUmvtmTY
-            Console.WriteLine(publicKey.GetAddress(Network.TestNet)); // n3zWAo2eBnxLr3ueohXnuAa8mTVBhxmPhq
 
+
+            //===========================================================================================
+            //Section2. Dive into more details of "new Key()", a private key, a public key.
+
+            //Yes, it is a key object. If you print a privateKey variable by:
+            Console.WriteLine(privateKey);
+
+            
+            //============================================================================================
+            //Section3. Bitcoin network and Bitcoin address
+
+            Console.WriteLine(publicKey.GetAddress(Network.Main));
+            Console.WriteLine(publicKey.GetAddress(Network.TestNet));
+            
+            //The above illustration shows a standard way of generating a Bitcoin address by processing entire steps(Public key -> Public key hash +Network => Bitcoin address), with not using a sugar syntax for generating a Bitcoin address(Public key + Network => Bitcoin address).
             var publicKeyHash = publicKey.Hash;
-            Console.WriteLine(publicKeyHash); // f6889b21b5540353a29ed18c45ea0031280c42cf
+            Console.WriteLine(publicKeyHash);
+            
+            //Get a Bitcoin address for a mainnet by publicKeyHash and network identifier.
             var mainNetAddress = publicKeyHash.GetAddress(Network.Main);
             var testNetAddress = publicKeyHash.GetAddress(Network.TestNet);
+            Console.WriteLine(mainNetAddress);
+            Console.WriteLine(testNetAddress);
 
-            Console.WriteLine(mainNetAddress); // 1PUYsjwfNmX64wS368ZR5FMouTtUmvtmTY
-            Console.WriteLine(testNetAddress); // n3zWAo2eBnxLr3ueohXnuAa8mTVBhxmPhq
+
+            //============================================================================================
+            //Section4. Encoding schemes
+            
+            //The Base58Check encoding scheme also provides a consistent way to determine the network of a given address, which means that this feature prevents a wallet from sending MainNet coins to a TestNet address.
+            var bitcoinAddressForMainNet = privateKey.PubKey.GetAddress(Network.Main);
+            var bitcoinAddressForTestNet = privateKey.PubKey.GetAddress(Network.TestNet);
+
+            //Get the consistent network type from the specific Bitcoin address.
+            var mainNetFromBitcoinAddress = bitcoinAddressForMainNet.Network;
+            var testNetFromBitcoinAddress = bitcoinAddressForTestNet.Network;
+            Console.WriteLine($"mainNetFromBitcoinAddress: {mainNetFromBitcoinAddress}");
+            Console.WriteLine($"testNetFromBitcoinAddress: {testNetFromBitcoinAddress}");
             
             Console.ReadLine();
         }
