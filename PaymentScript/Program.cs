8568a0bbbcf16f1b9c0def0e3c9b936e48bd7972
Young-min Park <youngmtool@gmail.com>
2017-11-28 10:45:36 +0900

    Update Program.cs

--- PaymentScript/Program.cs
+++ PaymentScript/Program.cs
@@ -8,26 +8,78 @@ namespace PaymentScript
     {
         static void Main()
         {
-            var publicKeyHash = new KeyId("14836dbe7f38c5ac3d49e8d790af808a4ee9edcf");
+            RandomUtils.Random = new UnsecureRandom();
 
-            var testNetAddress = publicKeyHash.GetAddress(Network.TestNet);
-            var mainNetAddress = publicKeyHash.GetAddress(Network.Main);
-
-            Console.WriteLine(mainNetAddress.ScriptPubKey); // OP_DUP OP_HASH160 14836dbe7f38c5ac3d49e8d790af808a4ee9edcf OP_EQUALVERIFY OP_CHECKSIG
-            Console.WriteLine(testNetAddress.ScriptPubKey); // OP_DUP OP_HASH160 14836dbe7f38c5ac3d49e8d790af808a4ee9edcf OP_EQUALVERIFY OP_CHECKSIG
+            //===========================================================================================
+            //Chapter3. ScriptPubKey
 
             
-            var paymentScript = publicKeyHash.ScriptPubKey;
-            var sameMainNetAddress = paymentScript.GetDestinationAddress(Network.Main);
-            Console.WriteLine(mainNetAddress == sameMainNetAddress); // True
+            BitcoinSecret bitcoinSecretForThisExample = new BitcoinSecret("L5DZpEdbDDhhk3EqtktmGXKv3L9GxttYTecxDhM5huLd82qd9uvo", Network.Main);
+            Console.WriteLine($"bitcoinSecretForThisExample: {bitcoinSecretForThisExample}");
+            
+            
+
+            Key privateKeyForThisExample = bitcoinSecretForThisExample.PrivateKey;
+            Console.WriteLine($"privateKeyForThisExample: {privateKeyForThisExample}");
+            
+            
+
+            var publicKeyForThisExample = privateKeyForThisExample.PubKey;
+            Console.WriteLine($"publicKeyForThisExample: {publicKeyForThisExample}");
+            
+            
+
+            var publicKeyHashForThisExample = publicKeyForThisExample.Hash;
+            Console.WriteLine($"publicKeyHashForThisExample: {publicKeyHashForThisExample}");
+            
+            
 
 
-            var samePublicKeyHash = (KeyId)paymentScript.GetDestination();
-            Console.WriteLine(publicKeyHash == samePublicKeyHash); // True
-            var sameMainNetAddress2 = new BitcoinPubKeyAddress(samePublicKeyHash, Network.Main);
-            Console.WriteLine(mainNetAddress == sameMainNetAddress2); // True
+            //For a reference, this is the way of generating a public key directly by passing the public key hash string value (97eb9da945f16139acb552a2de4081eb7f5176d9) which you want to use as the publich key hash.
+            //This generates a public key hash directly to have a hardcoded value.
+            var publicKeyHashByHardCodedValue = new KeyId("97eb9da945f16139acb552a2de4081eb7f5176d9");
+            Console.WriteLine($"publicKeyHashBySimpleWay: {publicKeyHashByHardCodedValue}");
+            
+            
 
 
+            //Generates a bitcoin address for the specific network.
+            var bitcoinAddressForTestNet = publicKeyHashForThisExample.GetAddress(Network.TestNet);
+            var bitcoinAddressForMainNet = publicKeyHashForThisExample.GetAddress(Network.Main);
+
+            Console.WriteLine($"bitcoinAddressForTestNet {bitcoinAddressForTestNet}");
+            Console.WriteLine($"bitcoinAddressForMainNet {bitcoinAddressForMainNet}");
+
+
+
+
+
+            //Generate a ScriptPubKey from a Bitcoin address.
+            Console.WriteLine(bitcoinAddressForMainNet.ScriptPubKey);
+            Console.WriteLine(bitcoinAddressForTestNet.ScriptPubKey);
+            
+
+
+            //Generate a ScriptPubKey from publicKeyHash.
+            var scriptPubKeyForPayment = publicKeyHashForThisExample.ScriptPubKey;
+            //Get a Bitcoin address by specifying a network identifier on the ScriptPubKey.
+            var bitcoinAddressFromSPKForMainNet = scriptPubKeyForPayment.GetDestinationAddress(Network.Main);
+            Console.WriteLine(bitcoinAddressForMainNet == bitcoinAddressFromSPKForMainNet);
+            
+
+
+            
+
+            //Get the public key hash from the ScriptPubKey.
+            var publicKeyHashFromSPK = (KeyId)scriptPubKeyForPayment.GetDestination();
+            Console.WriteLine(publicKeyHashForThisExample == publicKeyHashFromSPK); 
+            
+            
+
+            //Get the Bitcoin address by using the publick key hash retrieved from the ScriptPubKey from above code and network identifier.
+            var bitcoinAddressGotAgain = new BitcoinPubKeyAddress(publicKeyHashFromSPK, Network.Main);
+            Console.WriteLine(bitcoinAddressForMainNet == bitcoinAddressGotAgain);
+            
             Console.ReadLine();
         }
     }
